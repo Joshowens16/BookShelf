b62227baf1cc1a560a0574d44337f6ad2f4bf3bf
Joshowens16 <joshowens16@gmail.com>
2022-11-21 12:27:03 -0800

    basic server set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Route, Routes, Link } from "react-router-dom";
+import LandingPage from "./Components/Home/LandingPage.jsx";
 function App() {
-  return <div className="App">HELLO WORLD</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Home/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LandingPage = () => {
+    return (
+        <div>
+            Welcome to Bookshelf!
+        </div>
+    );
+};
+
+export default LandingPage;
